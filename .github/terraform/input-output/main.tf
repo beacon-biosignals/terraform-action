7e36e0baeb96cf7986d16f25a5b64221de491352
Curtis Vogt <curtis.vogt@gmail.com>
2025-03-31 08:42:34 -0500

    fixup! Refactor integration tests

---
+++ .github/terraform/input-output/main.tf
@@ -0,0 +1,21 @@
+variable "required" {
+  type = object
+}
+
+variable "optional" {
+  type    = string
+  default = null
+
+  validation {
+    condition = var.optional == null || var.optional == "foo"
+    error_message = "`optional` must only be `null` or `\"foo\"`."
+  }
+}
+
+output "required" {
+  value = var.required
+}
+
+output "optional" {
+  value = var.optional
+}
